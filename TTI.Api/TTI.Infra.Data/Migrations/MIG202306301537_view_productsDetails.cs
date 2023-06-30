76ea7663c53ea6d0622edcfa863cd537605d3f40
Leonel Alexandre <leonelalexandre@hotmail.com>
2023-06-30 15:39:51 -0300

    migration view

---
+++ TTI.Api/TTI.Infra.Data/Migrations/MIG202306301537_view_productsDetails.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TTI.Infra.Data.Migrations
+{
+    public partial class MIG202306301537_view_productsDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE VIEW ProductsDetails AS
+                        SELECT ProductS.Name
+                        	   , Products.Currency
+                        	   , Products.Price
+                        	   , Products.Description
+                        	   , Categories.Description category
+                        	   , SubCategories.Description subCategory
+                        	   , Products.Country
+                        FROM PRODUCTS
+                        JOIN CATEGORIES ON CATEGORIES.ID = Products.IdCategory
+                        JOIN SubCategories ON SubCategories.Id = Categories.IdSubCategory";
+
+            migrationBuilder.Sql(sp);
+        }
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
